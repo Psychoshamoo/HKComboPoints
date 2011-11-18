8e1d7ddd756298e0d603f8b8e495f1bdd637301b
Psychoshamoo <dmk.24.48@gmail.com>
2011-11-18 15:03:19 -0600

    Added combo point frames

--- main.lua
+++ main.lua
@@ -1,8 +1,20 @@
 local FRAME_WIDTH = 200
-local FRAME_HEIGHT = 60
+local FRAME_HEIGHT = 40
 
 local frame = CreateFrame("Frame", "HelloKittyComboPointFrame", UIParent)
 frame.lastPoints = 0;
+
+for i = 1, 5 do
+	local childFrame = CreateFrame("Frame", "HelloKittyComboPoint"..i, frame)
+	childFrame:SetWidth(FRAME_HEIGHT)
+	childFrame:SetHeight(FRAME_HEIGHT)
+	childFrame:SetPoint("TOPLEFT", (i - 1) * FRAME_HEIGHT, 0)
+
+	local background = childFrame:CreateTexture("HelloKittyComboPoint"..i.."Background", "BACKGROUND")
+	background:SetTexture(1, 1, 1, 0.25)
+	background:SetAllPoints()
+	childFrame:Hide()
+end
 
 frame:SetWidth(FRAME_WIDTH)
 frame:SetHeight(FRAME_HEIGHT)
@@ -20,11 +32,21 @@ background:SetTexture(0, 0, 0, 1)
 background:SetAllPoints()
 ]]--
 
-frame:SetScript("OnUpdate", function(lastUpdate)
+frame:SetScript("OnUpdate", function(self, elapsed)
 	local cp = GetComboPoints("player", "target")
-	if cp ~= frame.lastPoints then
-		frame.lastPoints = cp
-		print(cp)
+	if cp ~= self.lastPoints then
+		self.lastPoints = cp
+
+		local points = { self:GetChildren() }
+		local i = 1;
+		for _, child in ipairs(points) do
+			if i <= cp then
+				child:Show()
+			else
+				child:Hide()
+			end
+			i = i + 1
+		end
 	end
 end)
 
